f14f5691b9071abf621eafa0d5464b13606f44a4
Jeff Oliver <jeffoliver3384@gmail.com>
2023-06-03 23:58:51 -0400

    LoginPage UI

---
+++ frontend/src/components/FormContainer.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+interface Props {
+	children: ReactNode;
+}
+
+const FormContainer = ({ children }: Props) => {
+	return (
+		<Container>
+			<Row className="justify-content-md-center mt-5">
+				<Col xs={12} md={6} className="card p-5">
+					{children}
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+export default FormContainer;
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,52 @@
+import { useState, FormEvent, ChangeEvent } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const LoginPage = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		console.log("submit");
+	};
+
+	return (
+		<FormContainer>
+			<h1>Sign In</h1>
+
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className="my-2" controlId="email">
+					<Form.Label>Email Address</Form.Label>
+					<Form.Control
+						type="email"
+						placeholder="Enter Email"
+						value={email}
+						onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group className="my-2" controlId="password">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Enter Password"
+						value={password}
+						onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+
+				<Button type="submit" variant="primary" className="mt-3">
+					Sign In
+				</Button>
+
+				<Row className="py-3">
+					<Col>
+						New Customer? <Link to="/register">Register</Link>
+					</Col>
+				</Row>
+			</Form>
+		</FormContainer>
+	);
+};
+export default LoginPage;
