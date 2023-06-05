e80dd022b003c0b3d7438878d8207cb31574dc72
Jeff Oliver <jeffoliver3384@gmail.com>
2023-06-04 22:50:26 -0400

    API slices

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { CurriedGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
 
 import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		[apiSlice.reducerPath]: apiSlice.reducer,
 	},
-	middleware: (getDefaultMiddleware: CurriedGetDefaultMiddleware) => getDefaultMiddleware(),
+	middleware: (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
+		getDefaultMiddleware().concat(apiSlice.middleware),
 	devTools: true,
 });
 
